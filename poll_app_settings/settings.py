f571c2c7e74f0bac68f45c50a5a97d4bc0e0e24a
Gor <gorsargsyanm@gmail.com>
2020-12-14 01:46:11 +0400

    static

--- poll_app_settings/settings.py
+++ poll_app_settings/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_URL = '=/staticfiles/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR / 'staticfiles')]
+STATIC_URL = '=/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
